c99c05a5ba137acd2bb9db74ef97b05b2ceb7809
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-03-10 14:11:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 double pown(double value, uint16_t n) {
     double result = 1.0;
     for(uint16_t i = 0; i < n; i++) {
-        result *= value; 
+        result *= value;
     }
     return result;
 }
@@ -20,32 +20,29 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-    double result = 0.0;
-    for(uint16_t i = 0; i < count; i++) {
-        result += calcItem(x, i);
-    } 
-    return result;
+    double resr = 1.0;
+    for (uint64_t i = 1; i <= count; i++) {
+        resr += calcItem(x, i);
+    }
+    return resr;
 }
 
 double sinn(double x, uint16_t count) {
-    double result = 0.0;
-    for(uint16_t i = 0; i < count; i++) {
-        double term = calcItem(x, 2 * i + 1);
-        result += (i % 2 == 0) ? term : (-term);
+    double resd = 0;
+    for (uint64_t i = 1; i <= count; i++) {
+        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 1);
     }
-    return result;
+    return resd;
 }
 
 double cosn(double x, uint16_t count) {
-    double result = 0.0;
-    result += 1.0;
-    for(uint16_t i = 1; i < count; i++) {
-        double term = calcItem(x, 2 * i);
-        result += (i % 2 == 0) ? term : (-term);
+    double resd = 0;
+    for (uint16_t i = 1; i <= count; i++) {
+        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 2);
     }
-    return result;
+    return resd;
 }
